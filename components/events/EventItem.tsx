514a22f5c8794324d659367282147f12c6bd8e73
Jeongeun-Choi <cje3080@gmail.com>
2023-01-03 01:43:55 +0900

    style: 스타일 적용

--- components/events/EventItem.tsx
+++ components/events/EventItem.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FeaturedEvent } from '../../pages/types';
 import Link from 'next/link';
+import classes from './event-item.module.css';
 
 const EventItem = ({ item }: { item: FeaturedEvent }) => {
   const hymanReadableDate = new Date(item.date).toLocaleDateString('en-US', {
@@ -12,19 +13,19 @@ const EventItem = ({ item }: { item: FeaturedEvent }) => {
   const formattedAddress = item.location.replace(', ', '\n');
   const exploreLink = `/events/${item.id}`;
   return (
-    <li>
+    <li className={classes.item}>
       <img src={`/${item.image}`} alt={item.title} />
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{item.title}</h2>
-          <div>
+          <div className={classes.date}>
             <time>{hymanReadableDate}</time>
           </div>
-          <div>
+          <div className={classes.address}>
             <address>{formattedAddress}</address>
           </div>
         </div>
-        <div>
+        <div className={classes.actions}>
           <Link href={exploreLink}>Explore Event</Link>
         </div>
       </div>
--- components/events/EventList.tsx
+++ components/events/EventList.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import EventItem from './EventItem';
 import { FeaturedEvent } from '../../pages/types';
+import classes from './event-list.module.css';
 
 const EventList = ({ items }: { items: FeaturedEvent[] }) => {
   return (
-    <ul>
+    <ul className={classes.list}>
       {items.map((item: FeaturedEvent) => (
         <EventItem key={item.id} item={item} />
       ))}
