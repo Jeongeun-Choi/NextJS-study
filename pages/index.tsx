60782adc11bee1c2d60d45f047dcc26d876dedb5
Jeongeun-Choi <cje3080@gmail.com>
2023-01-07 12:39:11 +0900

    add: getStaticProps 함수로 pre-rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,24 +4,39 @@ import { getFeaturedEvents } from '../dummy-data';
 import { FeaturedEvent } from './types';
 import EventList from '../components/events/EventList';
 
-function HomePage() {
-  const [featuredEvents, setFeaturedEvents] = useState<FeaturedEvent[]>([]);
+function HomePage(props) {
+  // const [featuredEvents, setFeaturedEvents] = useState<FeaturedEvent[]>([]);
 
-  const getEvents = useCallback(() => {
-    const featuredList = getFeaturedEvents();
+  // const getEvents = useCallback(() => {
+  //   const featuredList = getFeaturedEvents();
 
-    setFeaturedEvents(featuredList);
-  }, []);
+  //   setFeaturedEvents(featuredList);
+  // }, []);
 
-  useEffect(() => {
-    getEvents();
-  }, []);
+  // useEffect(() => {
+  //   getEvents();
+  // }, []);
+
+  const { products } = props;
 
   return (
-    <div>
-      <EventList items={featuredEvents} />
-    </div>
+    // <div>
+    //   <EventList items={featuredEvents} />
+    // </div>
+    <ul>
+      {products.map(product => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
   );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [{ id: 'p1', title: 'Product 1' }]
+    }
+  };
 }
 
 export default HomePage;
