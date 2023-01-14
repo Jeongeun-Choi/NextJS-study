230c5a36ba0466036feb52d3b84d09e836c438df
Jeongeun-Choi <cje3080@gmail.com>
2023-01-14 23:40:48 +0900

    fix: 페이지 라우트때 적용한 코드

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,51 +1,26 @@
-import Link from "next/link";
 import { useEffect, useCallback, useState } from "react";
 import { getFeaturedEvents } from "../dummy-data";
 import { FeaturedEvent } from "./types";
 import EventList from "../components/events/EventList";
 
-import fs from "fs/promises"; // React 앱 코드가 준비됐을때 NextJS 측에선 해당 코드를 삭제한다.
-import path from "path";
+function HomePage() {
+  const [featuredEvents, setFeaturedEvents] = useState<FeaturedEvent[]>([]);
 
-function HomePage(props) {
-  // const [featuredEvents, setFeaturedEvents] = useState<FeaturedEvent[]>([]);
+  const getEvents = useCallback(() => {
+    const featuredList = getFeaturedEvents();
 
-  // const getEvents = useCallback(() => {
-  //   const featuredList = getFeaturedEvents();
+    setFeaturedEvents(featuredList);
+  }, []);
 
-  //   setFeaturedEvents(featuredList);
-  // }, []);
-
-  // useEffect(() => {
-  //   getEvents();
-  // }, []);
-
-  const { products } = props;
+  useEffect(() => {
+    getEvents();
+  }, []);
 
   return (
-    // <div>
-    //   <EventList items={featuredEvents} />
-    // </div>
-    <ul>
-      {products.map((product) => (
-        <li key={product.id}>
-          <Link href={`/products/${product.id}`}>{product.title}</Link>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <EventList items={featuredEvents} />
+    </div>
   );
-}
-
-export async function getStaticProps() {
-  const filePath = path.join(process.cwd(), "dummy-backend.json");
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
-  return {
-    props: {
-      products: data.products,
-    },
-    revalidate: 600,
-  };
 }
 
 export default HomePage;
