b6c0525cea90afd84bf9e5ae9783a4cbd34343db
Jeongeun-Choi <cje3080@gmail.com>
2023-01-14 17:37:00 +0900

    add: getServerSideProps 관련 함수 테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import Link from 'next/link';
-import { useEffect, useCallback, useState } from 'react';
-import { getFeaturedEvents } from '../dummy-data';
-import { FeaturedEvent } from './types';
-import EventList from '../components/events/EventList';
+import Link from "next/link";
+import { useEffect, useCallback, useState } from "react";
+import { getFeaturedEvents } from "../dummy-data";
+import { FeaturedEvent } from "./types";
+import EventList from "../components/events/EventList";
 
-import fs from 'fs/promises'; // React 앱 코드가 준비됐을때 NextJS 측에선 해당 코드를 삭제한다.
-import path from 'path';
+import fs from "fs/promises"; // React 앱 코드가 준비됐을때 NextJS 측에선 해당 코드를 삭제한다.
+import path from "path";
 
 function HomePage(props) {
   // const [featuredEvents, setFeaturedEvents] = useState<FeaturedEvent[]>([]);
@@ -27,9 +27,9 @@ function HomePage(props) {
     //   <EventList items={featuredEvents} />
     // </div>
     <ul>
-      {products.map(product => (
+      {products.map((product) => (
         <li key={product.id}>
-          <Link href={`/${product.id}`}>{product.title}</Link>
+          <Link href={`/products/${product.id}`}>{product.title}</Link>
         </li>
       ))}
     </ul>
@@ -37,14 +37,14 @@ function HomePage(props) {
 }
 
 export async function getStaticProps() {
-  const filePath = path.join(process.cwd(), 'dummy-backend.json');
+  const filePath = path.join(process.cwd(), "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
   return {
     props: {
-      products: data.products
+      products: data.products,
     },
-    revalidate: 600
+    revalidate: 600,
   };
 }
 
--- pages/user-profile.js
+++ pages/user-profile.js
@@ -5,9 +5,14 @@ function UserPorfilePage(props) {
 export default UserProfilePage;
 
 export async function getServerSideProps(context) {
+  const { params, req, res } = context;
+
+  console.log(req);
+  console.log(res);
+
   return {
     props: {
-      userName: 'Luna'
-    }
+      userName: "Luna",
+    },
   };
 }
