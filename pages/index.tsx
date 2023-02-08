75aafe250bbd77330993e91b07e52f2439266305
Jeongeun-Choi <cje3080@gmail.com>
2023-02-08 20:14:09 +0900

    fix: 백엔드 코드 추가를 위한 파일 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,11 @@
-import Head from "next/head";
-import { useState } from "react";
-import { FeaturedEvent } from "./types";
-import EventList from "../components/events/EventList";
-import { getFeaturedEvents } from "../helpers/app-utils";
+import React from "react";
 
-function HomePage(props: { featuredData: FeaturedEvent[] }) {
-  const [featuredEvents, setFeaturedEvents] = useState<FeaturedEvent[]>(
-    props.featuredData
-  );
-
+const HomePage = () => {
   return (
     <div>
-      <Head>
-        <title>NextJS Events</title>
-        <meta name="description" content="I study NextJS :)"></meta>
-      </Head>
-      <EventList items={featuredEvents} />
+      <h1>The Home Page</h1>
     </div>
   );
-}
+};
 
 export default HomePage;
-
-export async function getServerSideProps() {
-  const featuredData = await getFeaturedEvents();
-
-  return {
-    props: { featuredData },
-    // revalidate: 1800,
-  };
-}
