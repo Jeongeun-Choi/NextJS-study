07513998704a64e0b0b23e8cd5525b8900605c73
Jeongeun-Choi <cje3080@gmail.com>
2023-02-19 22:43:46 +0900

    fix: api route 관련 프로젝트를 위한 컴포넌트 추가 및 index 파일 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,34 @@
-import React, { MouseEvent, useCallback, useRef, useState } from "react";
+import Head from "next/head";
+import { useState } from "react";
+import { FeaturedEvent } from "./types";
+import EventList from "../components/events/EventList";
+import { getFeaturedEvents } from "../helpers/app-utils";
+import NewsletterRegistration from "../components/input/newsletter-registration";
 
-const HomePage = () => {
-  const emailInputRef = useRef<HTMLInputElement>(null);
-  const feedbackInputRef = useRef<HTMLTextAreaElement>(null);
-  const [feedbackList, setFeedbackList] = useState<any[]>([]);
-
-  const handleSubmitFeedback = useCallback((e: MouseEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const emailValue = emailInputRef.current?.value;
-    const feedbackValue = feedbackInputRef.current?.value;
-
-    const bodyParams = { email: emailValue, feedback: feedbackValue };
-    fetch("/api/feedback", {
-      method: "POST",
-      body: JSON.stringify(bodyParams),
-      headers: {
-        "Content-type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, []);
-
-  const handleLoadFeedback = useCallback(() => {
-    fetch("/api/feedback")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setFeedbackList(data.feedback);
-      });
-  }, []);
+function HomePage(props: { featuredData: FeaturedEvent[] }) {
+  const [featuredEvents, setFeaturedEvents] = useState<FeaturedEvent[]>(
+    props.featuredData
+  );
 
   return (
     <div>
-      <h1>The Home Page</h1>
-      <form onSubmit={handleSubmitFeedback}>
-        <div>
-          <label htmlFor="email">Your Email Address</label>
-          <input ref={emailInputRef} type="email" id="email" />
-        </div>
-        <div>
-          <label htmlFor="feedback">Your Feedback</label>
-          <textarea ref={feedbackInputRef} id="feedback" rows={5} />
-        </div>
-        <button type="submit">Send Feedback</button>
-      </form>
-      <hr />
-      <button onClick={handleLoadFeedback}>Load Feedback</button>
-      <ul>
-        {feedbackList.map((feedback) => (
-          <li key={feedback.id}>{feedback.text}</li>
-        ))}
-      </ul>
+      <Head>
+        <title>NextJS Events</title>
+        <meta name="description" content="I study NextJS :)"></meta>
+      </Head>
+      <NewsletterRegistration />
+      <EventList items={featuredEvents} />
     </div>
   );
-};
+}
 
 export default HomePage;
+
+export async function getServerSideProps() {
+  const featuredData = await getFeaturedEvents();
+
+  return {
+    props: { featuredData },
+    // revalidate: 1800,
+  };
+}
