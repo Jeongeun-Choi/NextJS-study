42df744d0904c1a488d30904c57d896b811e8940
Jeongeun-Choi <cje3080@gmail.com>
2023-02-26 21:07:03 +0900

    add: 댓글 등록시 알림창 뜨도록 구현

--- components/input/comment-list.js
+++ components/input/comment-list.js
@@ -1,10 +1,13 @@
 import classes from "./comment-list.module.css";
 
-function CommentList({ items }) {
+function CommentList({ items, isLoading }) {
+  if (isLoading) {
+    return <div>댓글 등록중입니다...</div>;
+  }
   return (
     <ul className={classes.comments}>
       {/* Render list of comments - fetched from API */}
-      {items.map((comment) => (
+      {items?.map((comment) => (
         <li key={comment._id}>
           <p>{comment.text}</p>
           <div>
--- components/input/comments.js
+++ components/input/comments.js
@@ -1,20 +1,30 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 
 import CommentList from "./comment-list";
 import NewComment from "./new-comment";
 import classes from "./comments.module.css";
+import NotificationContext from "../../store/NotificationContext";
 
 function Comments(props) {
   const { eventId } = props;
 
   const [showComments, setShowComments] = useState(false);
   const [commentList, setCommentList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const notificationCtx = useContext(NotificationContext);
 
   function toggleCommentsHandler() {
     setShowComments((prevStatus) => !prevStatus);
   }
 
   function addCommentHandler(commentData) {
+    notificationCtx.showNotification({
+      title: "등록 중",
+      message: "댓글을 등록하고 있습니다.",
+      status: "pending",
+    });
+    setIsLoading(true);
     fetch(`/api/comments/regist/${eventId}`, {
       method: "POST",
       body: JSON.stringify({ params: commentData }),
@@ -22,9 +32,32 @@ function Comments(props) {
         "Content-type": "application/json",
       },
     })
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-    // send data to API
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+
+        return res.json().then((data) => {
+          throw new Error(data.message || "댓글 등록 실패");
+        });
+      })
+      .then(() => {
+        notificationCtx.showNotification({
+          title: "등록 성공",
+          message: "댓글을 등록하였습니다.",
+          status: "success",
+        });
+      })
+      .catch((error) => {
+        notificationCtx.showNotification({
+          title: "등록 실패",
+          message: error.message || "댓글 등록 실패",
+          status: "error",
+        });
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }
 
   const getCommentList = useCallback(() => {
@@ -45,7 +78,9 @@ function Comments(props) {
         {showComments ? "Hide" : "Show"} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList items={commentList} />}
+      {showComments && (
+        <CommentList items={commentList} isLoading={isLoading} />
+      )}
     </section>
   );
 }
